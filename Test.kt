55e4ada63fcc8305fbdbfe7ebed4b961baeb0936
scshin <blueskyroad@naver.com>
2023-07-21 00:58:33 +0900

    git commit -m "lv2까지 완료"

---
+++ Test.kt
@@ -0,0 +1,16 @@
+package com.example.myapplication
+
+fun main() {
+
+    var infos = arrayOf("꿩", "닭", "참새", "오목눈이", "공작새")
+
+    for(info in infos) {
+
+        if(info == "참새") {
+            println("찾았다 참새!")
+            break
+        } else {
+            continue
+        }
+    }
+}
